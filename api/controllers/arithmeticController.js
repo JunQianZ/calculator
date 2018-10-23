5cb2218a9feffd3ea20bf59eefce551d1744f1d6
Zhuang <asks_zhuang@hotmail.com>
2018-10-23 21:46:33 +0800

    Update arithmeticController.js
    update js file

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -38,4 +38,41 @@ exports.calculate = function(req, res) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
 
-  if (! req.query.operand2 ||
+  if (! req.query.operand2 ||    
+      ! req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) || 
+
+
+
+      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) { 
+
+
+
+    throw new Error("Invalid operand2: " + req.query.operand2); 
+
+
+
+  } 
+
+
+
+ 
+
+
+
+  var operand1 = parseInt(req.query.operand1, 10); 
+
+
+
+  var operand2 = parseInt(req.query.operand2, 10); 
+
+
+
+ 
+
+
+
+  res.json({ result: operation(req.query.operand1, req.query.operand2) }); 
+
+
+
+};
